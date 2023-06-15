8fe0e50c53b423a832c745eb961d5db5b0898c79
mega-mind01 <muyiwameleki@gmail.com>
2023-06-15 07:31:30 -0700

    101 c language task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: return 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
