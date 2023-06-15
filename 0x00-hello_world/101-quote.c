688f8f4c97bc2c739e53062aed2ef99e64f95b21
mega-mind01 <muyiwameleki@gmail.com>
2023-06-15 08:33:15 -0700

    C language task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Entry point of the program
@@ -8,7 +9,7 @@
 
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
