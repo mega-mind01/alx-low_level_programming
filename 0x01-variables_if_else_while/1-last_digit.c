50694b0d2c2b3a4823fe46c1fd30862482556b74
mega-mind01 <muyiwameleki@gmail.com>
2023-06-16 07:34:31 -0700

    1 task modulous operation

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,14 +15,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int x = n % 10;
+	printf("Last digit of %d is %d", n, n % 10);
 
-	printf("Last digit of %d is %d", n, x);
-
-	if (x > 5)
+	if (n % 10 > 5)
 	{
 		printf("and is greater than 5\n");
-	} else if (x == 0)
+	} else if (n % 10 == 0)
 	{
 		printf("and is 0\n");
 	} else
