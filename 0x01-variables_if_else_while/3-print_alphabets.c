d8d70090c0c80c1335efb42b6dd72de690410f3e
mega-mind01 <muyiwameleki@gmail.com>
2023-06-16 08:21:08 -0700

    3 task double task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for program
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
