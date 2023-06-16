aa78bad409964e7ad3ab7643b41c730eb7f06b1c
mega-mind01 <muyiwameleki@gmail.com>
2023-06-16 08:45:29 -0700

    4 Task omitting char

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'e')
+		{
+			continue;
+		}
+		if (letter == 'q')
+		{
+			continue;
+		}
+
+		putchar(letter);
+	}
+	putchar('\n')
+	return (0);
+}
