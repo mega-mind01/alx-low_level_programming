8a703703662827013ba6838d890804c7764860b8
mega-mind01 <muyiwameleki@gmail.com>
2023-06-16 13:45:53 -0700

    102 C language task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,9 +11,9 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = i; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
