f0c187bbcad0ece49899814e219f65cf10876754
mega-mind01 <muyiwameleki@gmail.com>
2023-06-16 10:16:36 -0700

    tweaking with putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point of program
+ *
+ *  Return: return 0
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number + '0');
+
+		if (number != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
