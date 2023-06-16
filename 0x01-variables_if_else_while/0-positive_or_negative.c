e9b40586ac08878a3da13b31f7df84b79b1299e8
mega-mind01 <muyiwameleki@gmail.com>
2023-06-16 06:20:01 -0700

    0 task if statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,13 +17,13 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("is positive");
+		printf("%d is positive\n", &n);
 	} else if (n < 0)
 	{
-		printf("is negative");
+		printf("%d is negative\n", &n);
 	} else
 	{
-		printf("is zero");
+		printf("%d is zero\n", &n);
 	}
 	return (0);
 }
