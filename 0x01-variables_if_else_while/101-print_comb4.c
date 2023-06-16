e7de9710f6b9986c31f1ee3d5415bbd481bd147b
mega-mind01 <muyiwameleki@gmail.com>
2023-06-16 12:12:03 -0700

    101 Tripple digit manipulation

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,9 +12,9 @@ int main(void)
 	int j;
 	int k;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i <= 7; i++)
 	{
-		for (j = i + 1; j < 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			for (k = j + 1; k <= 9; k++)
 			{
