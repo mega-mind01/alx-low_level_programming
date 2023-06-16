7c439aa340967a89f3e6fbf378881a367000687e
mega-mind01 <muyiwameleki@gmail.com>
2023-06-16 07:53:18 -0700

    2 new task output with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	(for letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+		putchar(\n);
+	}
+
+	return (0);
+}
