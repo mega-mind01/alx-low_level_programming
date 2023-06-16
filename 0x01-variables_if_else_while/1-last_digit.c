c0edee0e6ebf899035d0c3c4b85e0c8fc22f80e5
mega-mind01 <muyiwameleki@gmail.com>
2023-06-16 07:27:16 -0700

    1 task moduloud and if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,13 +21,13 @@ int main(void)
 
 	if (x > 5)
 	{
-		printf("and is greater than 5\n", n, x);
+		printf("and is greater than 5\n");
 	} else if (x == 0)
 	{
-		printf("and is 0\n", n, x);
+		printf("and is 0\n");
 	} else
 	{
-		printf("and is less than 6 and not 0\n", n, x);
+		printf("and is less than 6 and not 0\n");
 	}
 
 	return (0);
