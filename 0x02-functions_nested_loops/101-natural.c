1031e00fde85236d17b324e391b39f39ee8dac9d
mega-mind01 <muyiwameleki@gmail.com>
2023-06-20 14:22:15 -0700

    101 Task on c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0  (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
+}
