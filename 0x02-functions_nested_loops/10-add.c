10379fb45f66af42d8040f7c5ff8dc965428ad18
mega-mind01 <muyiwameleki@gmail.com>
2023-06-20 13:08:02 -0700

    Additions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ *
+ * @i: First input
+ * @j: Second input
+ *
+ * Return: returns sum
+ */
+
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+
+	return (k);
+}
