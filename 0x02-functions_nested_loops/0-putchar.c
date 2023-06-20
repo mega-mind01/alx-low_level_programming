47dac562c4dce24b8b23b1eec23463800455a7d4
mega-mind01 <muyiwameleki@gmail.com>
2023-06-20 03:20:40 -0700

    0-putchar.c task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char j[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(j[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
