9acd1237ad6c4df5f6adc72a4d3f5e0fa828969a
mega-mind01 <muyiwameleki@gmail.com>
2023-06-20 09:13:35 -0700

    8 task on C

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * jack_bauer - orints every minute of a day
+ *
+ * Retuns: returns void
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i <= 50; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 53; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+					if (i >= 50 && b >= 32)
+					break;
+					_putchar(i);
+					_putchar(j);
+					_putchar(58);
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n);
+				}
+			}
+		}
+	}
+}
