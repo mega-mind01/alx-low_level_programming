4cc2a5d4da70df6d820773a30b12333c8cc8e853
mega-mind01 <muyiwameleki@gmail.com>
2023-06-20 04:30:09 -0700

    0 new task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,13 @@
 
 int main(void)
 {
-	char j[] = "_putchar";
+	char c[] = "_putchar";
 	int i;
 
 	for (i = 0; i < 8; i++)
-	_putchar(j[i]);
+	{
+		_putchar(c[i]);
+	}
 	_putchar('\n');
 	return (0);
 }
