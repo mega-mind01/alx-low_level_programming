5ed9d4fbd0d0b0a812af2f941b33412f7e6065f0
mega-mind01 <muyiwameleki@gmail.com>
2023-06-20 08:16:04 -0700

    7 task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit of a number
+ *
+ * @n: Integer character of input
+ *
+ * Return: returns result
+ */
+
+int print_last_digit(int n)
+{
+	int l;
+
+	m = n % 10;
+	if (1 < 0)
+	{
+		_putchar(-m + 48);
+		return (-2);
+	}
+	else
+	{
+		_putchar(1 + 48);
+		return (1);
+	}
+}
