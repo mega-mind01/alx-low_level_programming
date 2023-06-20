31e3000c745d9d9fe688984af505eddee4da5268
mega-mind01 <muyiwameleki@gmail.com>
2023-06-20 07:14:27 -0700

    6 task on function

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,14 +3,12 @@
 /**
  * _abs - Prints absolute value of an integer
  *
- * @int: integer value of input
+ * @i: integer value of input
  * Return: The absolute value
  */
 
-int _abs(int)
+int _abs(int i)
 {
-	int i;
-
 	if (i >= 0)
 	{
 		return (i);
