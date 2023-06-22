07f81263269e12ea1f7a884eb09a9597845ebe7e
mega-mind01 <muyiwameleki@gmail.com>
2023-06-22 10:41:08 -0700

    i doubt it

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,9 @@
 
 void print_line(int n)
 {
-	int i;
+	char i;
 
-	for (i < n; i <= n; i++)
-	{
+	for (i = 0; i < n; i++)
 		_putchar('_');
-	}
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
