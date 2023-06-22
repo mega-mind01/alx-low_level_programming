4fb60adab230788eb2ab6c542b344928a69f9b87
mega-mind01 <muyiwameleki@gmail.com>
2023-06-22 11:02:58 -0700

    Done again

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,12 +10,19 @@
 
 void print_diagonal(int n)
 {
-	char c;
-	char d;
-
-	for (c = 0; c < n; c++)
-		_putchar(' ');
-	_putchar('\\');
-	_putchar('\n');
+	if (n <= 0)
+	{
+		_putcar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('\\');
+			}
+			_putchar('\n');
+		}
+	}
 }
-
