8fd96c90e8cbc080e13caab704fa68b452e4d764
mega-mind01 <muyiwameleki@gmail.com>
2023-06-22 10:09:23 -0700

    new line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ *
+ * Return: returns void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= 6; i++)
+	{
+		_putchat('_');
+	}
+	_putchar('\n');
+}
