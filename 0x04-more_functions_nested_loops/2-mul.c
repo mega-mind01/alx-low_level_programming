d346b0a707e2ae69e2c38b8bb4a1dc93924f9857
mega-mind01 <muyiwameleki@gmail.com>
2023-06-22 07:26:45 -0700

    we multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Calculates the multiplication of two integers
+ *
+ * @a: integer input 1
+ * @b: integer input 2
+ *
+ * Return: returns sum 
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return (sum);
+}
