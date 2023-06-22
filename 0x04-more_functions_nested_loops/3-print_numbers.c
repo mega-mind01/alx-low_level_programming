6635c3aac8a7923d6be1a13bd053e7dd8d29676b
mega-mind01 <muyiwameleki@gmail.com>
2023-06-22 07:37:14 -0700

    3 Task to print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - returns value of 0-9
+ *
+ * Return: returns int
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		return (i);
+	}
+	_putchar('\n');
+}
