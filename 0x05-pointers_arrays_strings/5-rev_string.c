c5b870866b349d37425d38370a96673375f73057
mega-mind01 <muyiwameleki@gmail.com>
2023-06-26 07:59:56 -0700

    5 task on pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *rev_string - reverse given string
+ *
+ *@s: string input
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
