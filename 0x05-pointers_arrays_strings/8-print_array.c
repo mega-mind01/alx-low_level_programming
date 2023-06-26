f9e7df3e3d5d544abe554b89f5fccd2df2eeec0c
mega-mind01 <muyiwameleki@gmail.com>
2023-06-26 11:47:01 -0700

    new task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * print_array - prints an array
+ *
+ *@a: array name
+ *@n: index
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	printf("%d", a[0]);
+
+	for (i = 1; i < n; i++)
+	{
+		printf(", %d", a[i]);
+	}
+	printf("\n");
+}
