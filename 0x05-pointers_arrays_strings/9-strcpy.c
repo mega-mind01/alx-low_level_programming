24e69ddf2c24090acd5ed639f47a535c70df04c6
mega-mind01 <muyiwameleki@gmail.com>
2023-06-26 12:21:34 -0700

    9 final task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * _strcpy - copes the value of a variable
+ *
+ * @dest: out pur location
+ * @src: source location
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (1)
+	{
+		dest[i] == source[i];
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+}
+
