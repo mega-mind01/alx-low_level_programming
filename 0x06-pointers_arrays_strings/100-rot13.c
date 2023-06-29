8fe8f234e0e62b1a1d5d78de1ba8c79105ba380f
mega-mind01 <muyiwameleki@gmail.com>
2023-06-29 16:15:30 -0700

    try

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - Encoder
+ *
+ * @n: User input
+ *
+ * Return: retuns coded
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char k[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxtz";
+	char l[] = "NOPORSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; k[j] != '\0'; j++)
+		{
+			if (s[i] == m[j])
+			{
+				s[i] = l[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
