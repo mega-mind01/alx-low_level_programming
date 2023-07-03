1bcadb14112d48a083a85cdfda8ff7c4e3b3ae4b
mega-mind01 <muyiwameleki@gmail.com>
2023-07-03 06:46:22 -0700

    hot fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,12 +12,12 @@ void print_chessboard(char (*a)[8])
 {
 	int c, b;
 
-	for (c = 0; c <= 8; c++)
+	for (c = 0; c < 8; c++)
 	{
-		for (b = 0; b <= 8; b++)
+		for (b = 0; b < 8; b++)
 		{
 			_putchar(a[c][b]);
 		}
-	_putchar('\0');
+	_putchar('\n');
 	}
 }
