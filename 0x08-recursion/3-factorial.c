edcae30546b82366dcf899658da8c63653ed7c4b
mega-mind01 <muyiwameleki@gmail.com>
2023-07-05 05:23:36 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - solves for input factorial
+ *
+ * @n: argument
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
