77dfe5c7c2433fc789037a4a44ea28abd9755002
mega-mind01 <muyiwameleki@gmail.com>
2023-07-05 06:47:53 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * sqrtof - finds square root
+ *
+ * @a: input
+ * @b: matching int
+ * Return: int
+ */
+
+int sqrtof(int a, int b)
+{
+	if (n <= 0)
+		return (-1);
+	if (n * n == b)
+		return (a);
+	return (sqrtof(a - 1, b));
+}
+/**
+ * _sqrt_recursion - finds squareroot
+ *
+ * @n: int input
+ *
+ * Return: int value
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+		return (1);
+	return (sqrtof(n / 2, n));
+}
+
+/**
+ * is_prime_number - finds prime number
+ *
+ * @n: input
+ *
+ * Return: int value
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1 || _sqrt_recursion(n) >= 1)
+		return (0);
+	if (_sqrt_recursion(n) == -1)
+		return (1);
+	return (_sqrt_recursion(n));
+}
