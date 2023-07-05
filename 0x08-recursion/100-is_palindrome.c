8e8685a95ec3fad85349962219e503ff7b679e89
mega-mind01 <muyiwameleki@gmail.com>
2023-07-05 07:43:44 -0700

    100 recursive

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -25,15 +25,15 @@ int _strlen_recursion(char *s)
  * Return: 1 if equal 0 if not
  */
 
-int is_palid(char *a, int b, int c)
+int is_palid(char *s, int b, int c)
 {
 	if (b == c)
 		return (1);
 	if (b == c - 1)
-		return (a[b] == a[c]);
-	if (a[b] != a[c])
+		return (s[b] == s[c]);
+	if (s[b] != s[c])
 		return (0);
-	return (is_palid(a, b - 1, c - 1));
+	return (is_palid(s, b - 1, c - 1));
 }
 
 /**
