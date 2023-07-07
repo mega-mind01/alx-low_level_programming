06b3c139f4361b6b41e87c25df635f9eacd4f2f2
mega-mind01 <muyiwameleki@gmail.com>
2023-07-07 07:32:23 -0700

    1 argc task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main - Entry point of program
+ *
+ * @argc: Arguement to count
+ * @argv: Aguement of string
+ *
+ * Return: int 0 for success
+ */
+
+int main(int argc, char *argv)
+{
+	(void)argv;
+	printf("%d\n", argc -1);
+	return (0);
+}
