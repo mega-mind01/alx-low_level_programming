6b542dbb8537bab54faa44ceb7ba81c1d47479ef
mega-mind01 <muyiwameleki@gmail.com>
2023-07-07 06:52:36 -0700

    0 task on arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * @argc: Arguement for count
+ * @argv: Arguement for string
+ *
+ * Return: 0 for syccess
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argvv);
+	return (0);
+}
