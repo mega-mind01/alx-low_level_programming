fc66cf324f231a1d65f816596814491ec065bb18
mega-mind01 <muyiwameleki@gmail.com>
2023-07-11 04:20:21 -0700

    0 Task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array
+ *
+ * @size: byte size to allocate
+ * @c: Character value
+ *
+ * Return: Null if empty or pointer to address of array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	ptr = malloc(size * sizeof(char));
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
