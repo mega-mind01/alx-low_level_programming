a9bb97df8166df9e0f272d5dcd696c159534055d
mega-mind01 <muyiwameleki@gmail.com>
2023-07-11 06:41:27 -0700

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - free memory
+ *
+ * @grid: grid block to free
+ * @height: column to free
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[1]);
+	free(grid);
+}
