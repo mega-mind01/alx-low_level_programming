fddea1b822c57153ee4c68f7f15691d2444de02d
mega-mind01 <muyiwameleki@gmail.com>
2023-07-13 05:04:05 -0700

    tried

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2++;
 	if (n >= len2)
 		n = len2;
-	result = malloc(len1 + n + 1);
+	result = malloc(sizeof(char) * (len1 + n + 1));
 	if (result == NULL)
 		return (NULL);
 	while (s1[i] != '\0')
