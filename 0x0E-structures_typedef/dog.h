0ca4cffb4b37f379b7d9c31938435821c668c801
mega-mind01 <muyiwameleki@gmail.com>
2023-07-17 05:23:56 -0700

    first struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - contains details of dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
