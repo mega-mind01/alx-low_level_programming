75ad1e6254a809b36442d5c878573183dcf30b10
mega-mind01 <muyiwameleki@gmail.com>
2023-07-19 09:28:28 -0700

    multiple files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+#include "calc.h"
+
+/**
+ * main - calculates operations
+ *
+ * @argc: arguement count
+ * @argv: arguement passed
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv)
+{
+	int f1, f2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	f1 = atoi(argv[1]);
+	op = argv[2];
+	f2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (*op == '/' && f2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (*op == '%' && f2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(f1, f2));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "calc.h"
+
+/**
+ * op_add - add int
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: result of op
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract int
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: result of op
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * op_mul - multiplies int
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: result of op
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - divides int
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: result of op
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - checks for modulous b against a
+ * @a: num 1
+ * @b: num 2
+ *
+ * Return: result of op
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
