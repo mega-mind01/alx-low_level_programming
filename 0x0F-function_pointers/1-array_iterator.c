8e2b0e5218a368e53142594c0fd2cd64508ef901
mega-mind01 <muyiwameleki@gmail.com>
2023-07-19 04:49:03 -0700

    1 task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - loops over array and pass to function
+ * @array: array input
+ * @size: size of array
+ * @action: secondary function
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for(i = 0; i < size; i++)
+		action(array[i]);
+}
