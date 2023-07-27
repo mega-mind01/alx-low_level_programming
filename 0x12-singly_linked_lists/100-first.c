1aa2507ae64f690003cbc231165374bc9d740802
mega-mind01 <muyiwameleki@gmail.com>
2023-07-27 07:13:10 -0700

    prints before exe

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print_first(void)__attribute__((constructor));
+
+/**
+ * first - prints before execution
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
