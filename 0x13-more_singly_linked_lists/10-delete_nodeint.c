3f255e6fcf0c412df04bf2f7f3a340ecd04d9e98
mega-mind01 <muyiwameleki@gmail.com>
2023-08-01 14:04:04 -0700

    Hot fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (i < index - 1)
 	{
-		if (ptr == NULL || ptr->next == NULL)
+		if (!ptr || !(ptr->next))
 			return (-1);
 		ptr = ptr->next;
 		i++;
