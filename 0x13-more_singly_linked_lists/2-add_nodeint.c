1ac56cecf0e77e4acd7cf05633f692d8d0e30eba
mega-mind01 <muyiwameleki@gmail.com>
2023-08-01 04:43:48 -0700

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-		printf("Unable to alot memory");
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 
