1b5a7951f5105d018dc4ee462a5023f21476c6ef
mega-mind01 <muyiwameleki@gmail.com>
2023-08-01 05:13:58 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,12 +23,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
 
 	i = 1;
 	while (ptr != NULL)
