031168fd524831f3aceb31759960460d5c51b31a
mega-mind01 <muyiwameleki@gmail.com>
2023-08-03 21:50:09 -0700

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks for size of endian
+ *
+ * Return: 0 if big and 1 if little
+ */
+
+int get_endianness(void)
+{
+	unsigned int a = 1;
+	char *ptr = (char *)& a;
+
+	return (*c);
+}
