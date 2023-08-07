28f2a4fd3d853399cb15b374001e67b3f0a8de12
mega-mind01 <muyiwameleki@gmail.com>
2023-08-07 08:00:20 -0700

    Tasl 1 debug 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - creates a file
  *
- * @filename: file
+ * @filename: filie
  * @text_content: pointer to txt to write
  *
  * Return: 1 on success
@@ -17,10 +17,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (tet_content != NULL)
+	if (text_content != NULL)
 	{
-		for (count = 0; text_content[count];
-		count++;
+		for (count = 0; text_content[count];)
+			count++;
 	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
