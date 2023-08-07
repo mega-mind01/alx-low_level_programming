ec8373f16ca8d1a1407e3756ac4a8a85dc699bad
mega-mind01 <muyiwameleki@gmail.com>
2023-08-07 14:08:45 -0700

    Task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends a text to a file
+ *
+ * @filename: pointer to the text file to append
+ * @text_content: additional string input
+ *
+ * Return: as set
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	int write_file;
+	int count;
+
+	if (text_content != NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (count = 0; text_content[count];)
+			count++;
+	}
+
+	file = openr(filename, 0_WRONLY | 0_APPEND);
+	write_file = write(file, text_content, count);
+
+	if (file == -1 || write_file == -1)
+		return (-1);
+
+	close(file);
+
+	return (1);
+}
