cf7fb94d8cd0e6d0e305b3649e33311c2180ddcf
mega-mind01 <muyiwameleki@gmail.com>
2023-08-07 07:55:59 -0700

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: file
+ * @text_content: pointer to txt to write
+ *
+ * Return: 1 on success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int write_file;
+	int count;
+
+	if (filename == NULL)
+		return (-1);
+	if (tet_content != NULL)
+	{
+		for (count = 0; text_content[count];
+		count++;
+	}
+
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	write_file = write(file, text_content, count);
+
+	if (file == -1 || write_file == -1)
+		return (-1);
+	close(file);
+	return (1);
+}
